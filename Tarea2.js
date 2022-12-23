eaf74fb2447904787663bc2f9a4957a7bb7e2856
marsegovia <83015187+marsegovia@users.noreply.github.com>
2022-12-23 11:32:11 -0300

    Add files via upload

---
+++ Tarea2.js
@@ -0,0 +1,17 @@
+const Persona = 
+{
+    nombre: "Martin",
+    edad: 22,
+    desarrollador: true,
+    fecha_de_nacimiento: new Date(2000,9,10),
+    libro_favorito:
+     {
+     titulo: "Antologia Del Terror", 
+     autor:"H.P Lovercraft", 
+     fecha: new Date(), 
+     url: "www.yenny.com"
+
+     }
+
+}
+console.log(Persona);
