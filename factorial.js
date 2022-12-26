60d52e7a3f65e7b22d91a0f65dc4e33265a8a002
marsegovia <83015187+marsegovia@users.noreply.github.com>
2022-12-26 12:12:48 -0300

    Add files via upload

---
+++ factorial.js
@@ -0,0 +1,33 @@
+let factorial = 1;
+// factorial con FOR
+for(let i = 5; i > 0; i--)
+{
+    factorial *= i;
+}
+console.log(factorial);
+
+//factorial con WHILE
+let n=5, i=1, fact=1
+while(i<=n)
+{
+  fact = fact*i; 
+  i++;
+}
+console.log(fact);
+
+//factorial con WHILE, IF, BREAK
+let n1=5, i1=1, fact1=1
+while(i1 <=n1)
+{
+    fact1 = fact1*i1; 
+    i1++;
+    if (n1 < 0)
+    {
+    console.log("-1");
+    }
+    else if (n1 == 0) 
+    {
+    console.log("1");
+    }
+}
+console.log(fact1);
