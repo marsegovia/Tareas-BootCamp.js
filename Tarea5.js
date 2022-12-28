3debdd9bec3c73df0a828de9dea1abcda3e5df56
marsegovia <83015187+marsegovia@users.noreply.github.com>
2022-12-28 20:59:18 -0300

    Add files via upload

---
+++ Tarea5.js
@@ -0,0 +1,11 @@
+const altura = 170;
+const altura1 = 1.70;
+const peso = 69.900;
+altura_round = Math.round(x);
+peso_floor = Math.floor(peso);
+x = altura1.toFixed(2)
+ 
+console.log( "Altura:", altura + "cm");
+console.log("Altura:", altura_round + "m" );
+console.log("Peso:", peso + "Kg");
+console.log("Peso:", peso_floor +"Kg");
