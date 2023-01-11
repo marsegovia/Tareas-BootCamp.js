37ddca70625cb8a644daec934e07c6d2e01015aa
marsegovia <83015187+marsegovia@users.noreply.github.com>
2023-01-11 17:04:14 -0300

    Add files via upload

---
+++ sets.js
@@ -0,0 +1,13 @@
+const array = ["Martin","Patry","Seba","Chancho","Dobby"];
+const set = new Set(array);
+
+console.log(set);
+
+set.add("Seba");
+
+console.log(set);
+
+set.add("JavaScript");
+
+console.log(set);
+
